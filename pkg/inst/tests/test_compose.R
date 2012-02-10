ccd140f0828c0470d4b87dedcc9ef9df85b02299
Andrew Redd <amredd@gmail.com>
2012-02-09 19:53:38 -0700

    updated testing files

---
+++ pkg/inst/tests/test_compose.R
@@ -0,0 +1,6 @@
+test_that("Testing compose", {
+  expect_is(compose(any, is.na), 'function')
+  expect_error(compose(any, isna))
+  expect_true(compose(any,is.na)(NA))
+  expect_true((any%.%is.na)(NA))
+})
