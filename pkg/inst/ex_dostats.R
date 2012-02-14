0484445d5c04d6a04192972a3efb81e4d1290460
Andrew Redd <amredd@gmail.com>
2012-02-14 11:45:42 -0700

    removed files

--- pkg/inst/ex_dostats.R
+++
@@ -1,4 +0,0 @@
-data(mtcars)
-library(plyr)
-dostats(1:10, mean, median, sd, quantile, IQR)
-ldply(mtcars, dostats, median, mean, sd, quantile, IQR)
--- pkg/inst/tests/test_wargs.R
+++
@@ -1,3 +0,0 @@
-test_that("testing wargs", {
-  expect_true(!is.na(wargs(mean,na.rm=T)(c(NA, 1:3))))  
-})
